d250fb2db76f4b3c8ad26d1305be0ee66ac07ebb
runoob <2586084262@qq.com>
2018-10-22 14:52:18 +0800

    second commit

--- myDjango/MyBlog/url.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from MyBlog import views
-
-urlpatterns = [
-    path('blog/', views.get_blog, name='Blog'),
-    path('<int:blog_id>/detail/', views.get_details, name='blog_get_detail'),
-]
